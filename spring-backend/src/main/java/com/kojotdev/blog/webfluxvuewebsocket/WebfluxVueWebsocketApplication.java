f7f565fcd56c0899526cfcca806dd77e5c06327b
kojot <wojtek@herasoft.pl>
2019-08-07 14:39:45 +0200

    spring webflux initial project

---
+++ spring-backend/src/main/java/com/kojotdev/blog/webfluxvuewebsocket/WebfluxVueWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.kojotdev.blog.webfluxvuewebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxVueWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxVueWebsocketApplication.class, args);
+	}
+
+}
---
+++ spring-backend/src/test/java/com/kojotdev/blog/webfluxvuewebsocket/WebfluxVueWebsocketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kojotdev.blog.webfluxvuewebsocket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebfluxVueWebsocketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
