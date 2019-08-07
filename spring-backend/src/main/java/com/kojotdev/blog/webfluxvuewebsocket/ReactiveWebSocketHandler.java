933fa670d06d809cba66ea8a146224498df74321
kojot <wojtek@herasoft.pl>
2019-08-07 15:37:58 +0200

    plain text websocket handler

---
+++ spring-backend/src/main/java/com/kojotdev/blog/webfluxvuewebsocket/ReactiveWebSocketHandler.java
@@ -0,0 +1,22 @@
+package com.kojotdev.blog.webfluxvuewebsocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ReactiveWebSocketHandler implements WebSocketHandler {
+    @Override
+    public Mono<Void> handle(WebSocketSession webSocketSession) {
+        final Flux<WebSocketMessage> message = webSocketSession
+                .receive()
+                .map(webSocketMessage -> webSocketMessage.getPayloadAsText())
+                .map(name -> "Hello, " + name + "!")
+                .map(greetings -> webSocketSession.textMessage(greetings));
+
+        return webSocketSession.send(message);
+    }
+}
